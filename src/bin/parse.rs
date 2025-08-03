6e69d73476736a9890838f5f79fea82d5669fdcd
kariyayo <mail.bati11@gmail.com>
2025-08-03 10:32:48 +0900

    add sample c code

---
+++ src/bin/parse.rs
@@ -0,0 +1,15 @@
+use std::{fs::File, io::Read, path::Path};
+
+use ore_c_rust::create_parser;
+
+pub fn main() -> Result<(), std::io::Error> {
+    let file_path_str = "sample.c";
+    let path = Path::new(file_path_str);
+    let mut file = File::open(&path)?;
+    let mut input = String::new();
+    file.read_to_string(&mut input)?;
+    let mut parser = create_parser(&input);
+    let program = parser.parse_program();
+    println!("{:#?}", program);
+    return Ok(());
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 pub mod repl;
 mod lexer;
 mod parser;
+
+pub fn create_parser(input: &str) -> parser::Parser {
+    let l = lexer::Lexer::new(input);
+    let p = parser::Parser::new(l);
+    return p;
+}
