967365dd020773421acde6aadaf1fe8adc0f96ac
kariyayo <mail.bati11@gmail.com>
2026-01-03 17:11:38 +0900

    reuse Env from scope_checker

--- src/sema.rs
+++ src/sema.rs
@@ -6,7 +6,7 @@ mod semantic_checker;
 mod type_checker;
 
 pub fn semantic_analyze(ast: Program) {
-    let _env = scope_checker::check_scope(&ast);
+    let env = scope_checker::check_scope(&ast).unwrap();
     let _ = semantic_checker::check_semantic(&ast);
-    let _ = type_checker::check_type(&ast);
+    let _ = type_checker::check_type(&ast, &env);
 }
